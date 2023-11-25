a7ed52f85bc933a7996a34a5a9a0cc16dcfa6207
sduhubert <husyl23@student.sdu.dk>
2023-11-25 18:07:50 +0100

    readability corrections, changes in room 0

--- src/Location.cs
+++ src/Location.cs
@@ -1,4 +1,6 @@
 ﻿using System.Runtime;
+using System.Runtime.CompilerServices;
+using WorldOfZuul;
 
 namespace foodman
 {
@@ -27,7 +29,7 @@ namespace foodman
                 Console.Write("[D] Try your luck with the Quiz ");
             }
             Console.WriteLine("[S] Leave the " + locName);
-            string answer = Console.ReadLine().ToLower();
+            string? answer = Console.ReadLine()?.ToLower();
             if (answer == "a")
             {
                 LookAround(locName, id);
@@ -65,76 +67,79 @@ namespace foodman
                     {
                         Console.WriteLine("You can smell expired food. Strange... Maybe you can ask someone about it?");
                         Console.WriteLine("[A] Ask the owner [Any Key] Ignore the smell");
-                        string read = Console.ReadLine().ToLower();
+                        string? read = Console.ReadLine()?.ToLower();
                         if (read == "a")
                         {
                             Console.WriteLine("Manager: 'The food is expired and ready to be thrown out. Yes, we will throw it to the trash. We don't recycle.'");
-                            Console.WriteLine(Player.name+": 'Recycling is actually very easy! Here, let me show you, so you will start doing so too!'");
-                            progress[0] += 1;
-                            //DON'T FORGET TO ADD NASTY FOOD TO INVENTORY
-                        } else
+                            Console.WriteLine($"{Player.name}: 'Recycling is actually very easy! Here, let me show you, so you will start doing so too!'");
+                            Items.FoodExpired.AddRandomAmount(1, 10);
+                            Items.FoodExpired.Display();
+                            progress[0]++;
+                        } 
+                        else
                         {
                             Console.WriteLine("You ignore the smell and decide to leave this disgusting place after getting yourself a few snacks");
-                            //DON'T FORGET TO ADD SNACKS TO INVENTORY AND TO REMOVE THE MONEY
+                            Items.Snacks.Add(1);
+                            //REMOVE THE MONEY
                         }
                     }
                     else if (progress[id] == 1)
                     {
                         Console.WriteLine("It is dark outside. The store will close soon. You take a look at the shelves. Some of them still have food. It is almost expired, so the store will throw it out. Do you want to take care of it?");
                         Console.WriteLine("[A] Take the leftovers [Any Key] Leave them there");
-                        string read = Console.ReadLine().ToLower();
+                        string? read = Console.ReadLine()?.ToLower();
                         if (read == "a")
                         {
                             Console.WriteLine("You take the food. Now you can either eat it or sell it for 1/4 of its original price");
                             progress[0] += 1;
-                            //DON'T FORGET TO ADD WORSE FOOD TO INVENTORY
                         }
                         else
                         {
                             Console.WriteLine("You ignore the food. It is best to throw things that have no use, right?");
                         }
-                    } else
+                    } 
+                    else
                     {
                         Console.WriteLine("There is nothing left for you to do here...");
                         Console.WriteLine("[Any Key] Exit");
                         //Leaving
                     }
-                break;
+                    break;
                 case 1:
                     //restaraunt questline
                     break;
                     //other locations will have the same thing. Such individuality allows us to make every quest unique
                 case 2:
-                string currentYear = "";
-                 Console.WriteLine("Welcome to the House!");
-                 Console.ReadLine();
-                 Console.WriteLine("└|*-*|┘ - 'Hi! My name's Robert the Robot and I arrive from future, when I am your typical Eco-Friendly Robot assisting with everyday tasks.'");
-                 Console.ReadLine();
-                 Console.WriteLine("└|*-*|┘ - 'People of future are rarely needing my help as years of education throughout the FOODMAN have resulted in eco-aware society, that just knows how to waste less food.'");
-                 Console.ReadLine();
-                 Console.WriteLine("└|*-*|┘ - 'Even though I love humans of my times, I'd lie saying that I don't miss when they were less educated and needed my assistance more. That's why after consulting with them they've decided to try sending me back in time, so I could educate people of the past.'");
-                 Console.ReadLine();
-                 Console.WriteLine("└|*-*|┘ - 'Oh wow I think they did succeed right? Could you please tell me what year are we in?'");
-                 currentYear = Console.ReadLine();
-                 Console.WriteLine($"└|*-*|┘ - '{currentYear}?!?'");
-                 Console.ReadLine();
-                 Console.WriteLine("└|*-*|┘ - 'That is crazy!'");
-                 Console.ReadLine();
-                 Console.WriteLine("└|*-*|┘ - 'So it did work after all... Amazing! By the way excuse my poor manners, your name is ",Player.name+" right?'");
-                 Console.ReadLine();
-                 Console.WriteLine($"└|*-*|┘ - 'You look like a {Player.name}.'");
-                 Console.ReadLine();
-                 Console.WriteLine("└|*-*|┘ - 'OK, now that we know each other would you be interested in participating in a quizz that would evaluate your knowledge regarding ways of lessening food waste? Type YES if you would like to try, or NO if you would rather go on exploring other rooms.'");
-                 string decisionQuizz = Console.ReadLine().ToLower();
-                 
-                 if(decisionQuizz == "yes")
-                 {
-                   Console.WriteLine("└|*-*|┘ - 'I AM SO EXCITED!!!!! Let's begin then.'");
-                   //Quizz();
-                 } else if(decisionQuizz == "no")
-                 {
-                    Console.WriteLine("└|*-*|┘ - 'Fine, no worries. In case you would change your mind, please do not hesitate to come back, I'll be waiting.'");
-                 }
+                string? currentYear = "";
+                Console.WriteLine("Welcome to the House!");
+                Console.ReadLine();
+                Console.WriteLine("└|*-*|┘ - 'Hi! My name's Robert the Robot and I arrive from future, when I am your typical Eco-Friendly Robot assisting with everyday tasks.'");
+                Console.ReadLine();
+                Console.WriteLine("└|*-*|┘ - 'People of future are rarely needing my help as years of education throughout the FOODMAN have resulted in eco-aware society, that just knows how to waste less food.'");
+                Console.ReadLine();
+                Console.WriteLine("└|*-*|┘ - 'Even though I love humans of my times, I'd lie saying that I don't miss when they were less educated and needed my assistance more. That's why after consulting with them they've decided to try sending me back in time, so I could educate people of the past.'");
+                Console.ReadLine();
+                Console.WriteLine("└|*-*|┘ - 'Oh wow I think they did succeed right? Could you please tell me what year are we in?'");
+                currentYear = Console.ReadLine();
+                Console.WriteLine($"└|*-*|┘ - '{currentYear}?!?'");
+                Console.ReadLine();
+                Console.WriteLine("└|*-*|┘ - 'That is crazy!'");
+                Console.ReadLine();
+                Console.WriteLine("└|*-*|┘ - 'So it did work after all... Amazing! By the way excuse my poor manners, your name is ",Player.name+" right?'");
+                Console.ReadLine();
+                Console.WriteLine($"└|*-*|┘ - 'You look like a {Player.name}.'");
+                Console.ReadLine();
+                Console.WriteLine("└|*-*|┘ - 'OK, now that we know each other would you be interested in participating in a quizz that would evaluate your knowledge regarding ways of lessening food waste? Type YES if you would like to try, or NO if you would rather go on exploring other rooms.'");
+                string? decisionQuizz = Console.ReadLine()?.ToLower();
+                
+                if(decisionQuizz == "yes")
+                {
+                  Console.WriteLine("└|*-*|┘ - 'I AM SO EXCITED!!!!! Let's begin then.'");
+                  //Quizz();
+                } else if(decisionQuizz == "no")
+                {
+                   Console.WriteLine("└|*-*|┘ - 'Fine, no worries. In case you would change your mind, please do not hesitate to come back, I'll be waiting.'");
+                }
                 break;
 
             }
