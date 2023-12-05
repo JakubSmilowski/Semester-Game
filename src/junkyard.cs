3fbd096d37ee9358b36116a22d4c2fc5d9624b9e
Jakub Smilowski <jakub.smilowski@protonmail.com>
2023-12-05 18:18:47 +0100

    Added junkyard.cs

--- src/junkyard.cs
+++ src/junkyard.cs
@@ -1,26 +1,37 @@
 using System.Transactions;
 
-namespace foodman {
-    class Junkyard{
+namespace foodman
+{
+    class Junkyard
+    {
         //Main function caled after entering junkyard.
-        public static void EnterJunkyard(){
+        public static void EnterJunkyard()
+        {
 
-        }   
+        }
         //Draw Junkyard/
-        private static void DrawJunkyard(){
+        private static void DrawJunkyard()
+        {
 
         }
         //Message displayed only first day of the game/
-        private static void GreetingMessageStart(){
+        private static void GreetingMessageStart()
+        {
+            Console.WriteLine();
 
 
         }
         //Message dislayed after day one/
-        private static void GreetingMessageDefault(){
+        private static void GreetingMessageDefault()
+        {
+            Console.WriteLine();
+        }
+
+        public static void SellTheItem()
+        {
 
         }
 
-        
 
 
 
